e171d0094a9c56d9c4837d58eec8218dc925159d
Trahern <spammenotnameho@gmail.com>
2022-02-12 11:48:30 +0100

    some corrections

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+// Constants
+
+const PORT = process.env.PORT || 3001;
+const session = require('express-session');
+const express = require("express");
+var cors = require("cors");
+
+
+// App
+
+const app = express();
+
+app.use(session({
+            secret: 'secret string',
+            resave : false,
+            saveUninitialized : false,
+            cookie : {/* can add cookie related info here*/}
+        })
+      );
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true
+}));
+
+
+app.get('/', function(req,res){
+    if (!req.session.pageCountByCurrentUserOfAnyNameYouWant)
+        req.session.pageCountByCurrentUserOfAnyNameYouWant = 0;
+    req.session.pageCountByCurrentUserOfAnyNameYouWant++;
+    res.send({
+        pageCount : req.session.pageCountByCurrentUserOfAnyNameYouWant
+    });
+});
+
+
+app.listen(PORT, () => console.log(`The server is started on http://localhost:${PORT}`));
